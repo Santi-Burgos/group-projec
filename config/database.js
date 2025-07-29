992a83dd886cca9d1c10b82a0bbda8bd4bab23a4
Burgos <santiburgos38@gmail.com>
2025-07-29 12:39:28 -0300

    delete mysql2 add pg

--- config/database.js
+++ config/database.js
@@ -1,21 +1,17 @@
-import mysql from 'mysql2/promise';
-import {config as configDotenv} from 'dotenv';
+import { Pool } from 'pg';
+import { config as configDotenv } from 'dotenv';
 
 configDotenv();
 
-
-const connection = await mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,           
-    password: process.env.DB_PASS,           
-    database: process.env.DB_NAME, 
-    port: process.env.DB_PORT
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL, // URL completa desde Neon
+  ssl: {
+    rejectUnauthorized: false, // necesario para conexión segura en Neon
+  },
 });
-connection.getConnection((err, con) =>{
-    if(err){
-        console.log(`'no se puede conectar a a base de datos, ${error}'`)
-    }else{
-        console.log('conexion realizada con exito')
-    }
-})
-export default connection
+
+pool.connect()
+  .then(() => console.log('Conexión a PostgreSQL realizada con éxito'))
+  .catch((err) => console.error('No se pudo conectar a PostgreSQL', err));
+
+export default pool;
